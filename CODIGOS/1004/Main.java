d0a40643e804e3708165bc86d3192a273f29a0f4
sevak19 <tu1227csn@gmail.com>
2024-08-12 21:54:08 -0300

    upload 1004

---
+++ CODIGOS/1004/Main.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        int A, B, PROD;
+        
+        Scanner scan = new Scanner(System.in);
+        
+        A = scan.nextInt();
+ 
+        B = scan.nextInt();
+        
+        PROD = A * B;
+        
+        System.out.println("PROD = "+PROD);
+        
+        scan.close();
+        
+    }
+ 
+}
